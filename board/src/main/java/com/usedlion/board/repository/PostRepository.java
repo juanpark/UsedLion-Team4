9fd72d435b92930aa9e2ebc760be452be2a50bac
sihwan0816 <sihwan4472@daum.net>
2025-05-07 15:27:46 +0900

    refactor: Move board project under backend/board and remove old board/

--- board/src/main/java/com/usedlion/board/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.usedlion.board.repository;
-
-import com.usedlion.board.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
